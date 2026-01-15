9cb91f63a69fb1bb7d7cc7d654e265970648e59f
Sushanth <sushanthkartikeya18@gmail.com>
2026-01-15 21:39:25 +0530

    zod added

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,12 +2,18 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common";
 import { middleware } from "./middleware";
-
+import {CreateUserSchema,SigninSchema,CreateRoomSchema} from "@repo/common/types";
 const app = express();
 const PORT = 3001;
 
 app.post("/signin", (req, res) => {
-  
+    const data=SigninSchema.safeParse(req.body);
+     if(!data.success){
+          res.json({
+               message:"Incorrect data",
+          })
+          return;
+     }
     const userId=1;
     const token=jwt.sign({userId},JWT_SECRET);
 
@@ -15,10 +21,24 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/signup", (req, res) => {
+     const data=CreateUserSchema.safeParse(req.body);
+     if(!data.success){
+          res.json({
+               message:"Incorrect data",
+          })
+          return;
+     }
      res.json({userId:"123"})
 });
 
 app.post("/room",middleware,(req, res) => {
+     const data=CreateRoomSchema.safeParse(req.body);
+     if(!data.success){
+          res.json({
+               message:"Incorrect data",
+          })
+          return;
+     }
      res.json({
         roomId: "room-123",
      })
